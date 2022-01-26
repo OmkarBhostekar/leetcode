bd60e20e8ecc1646c0da00c66dacf4d262a27583
Omkar Bhostekar <59085919+OmkarBhostekar@users.noreply.github.com>
2022-01-26 23:55:37 +0530

    Time: 252 ms (22.71%), Space: 84.5 MB (79.25%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -18,28 +18,10 @@ public:
         inOrderTraversal(root->right,v);
     }
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int> ans;
-        vector<int> v1,v2;
-        inOrderTraversal(root1,v1);
-        inOrderTraversal(root2,v2);
-        int a=0,b=0;
-        while(a<v1.size() and b<v2.size()){
-            if(v1[a] < v2[b]){
-                ans.push_back(v1[a]);
-                a++;
-            }else{
-                ans.push_back(v2[b]);
-                b++;
-            }
-        }
-        while(a<v1.size()){
-            ans.push_back(v1[a]);
-            a++;
-        }
-        while(b<v2.size()){
-            ans.push_back(v2[b]);
-            b++;
-        }
-        return ans;
+        vector<int> v;
+        inOrderTraversal(root1,v);
+        inOrderTraversal(root2,v);
+        sort(v.begin(),v.end());
+        return v;
     }
 };
