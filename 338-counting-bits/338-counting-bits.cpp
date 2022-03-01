4e59f44b06ceb05c6956aa881aea08520e7e618d
Omkar Bhostekar <59085919+OmkarBhostekar@users.noreply.github.com>
2022-03-01 21:15:02 +0530

    Time: 3 ms (95.58%), Space: 7.9 MB (75.07%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,19 +1,10 @@
 class Solution {
-    int getOnesInBinary(int n){
-        int ans = 0;
-        while(n>0){
-            int r = n%2;
-            n /= 2;
-            if(r == 1)
-                ans++;
-        }
-        return ans;
-    }
 public:
     vector<int> countBits(int n) {
-        vector<int> ans;
-        for(int i=0; i<=n; i++){
-            ans.push_back(getOnesInBinary(i));
+        vector<int> ans(n+1);
+        ans[0] = 0;
+        for(int i=1; i<=n; i++){
+            ans[i] = ans[i/2]+i%2;
         }
         return ans;
     }
