297e25cf54a46e8637f32092b941863a53eaaa6f
Omkar Bhostekar <59085919+OmkarBhostekar@users.noreply.github.com>
2022-04-27 14:25:29 +0530

    Time: 19 ms (52.89%), Space: 13.5 MB (11.56%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& m) {
-        vector<vector<int>> temp = m;
-        for(int i=0;i<m.size();i++){
-            for(int j=0;j<m[0].size();j++){
-                if(temp[i][j] == 0){
-                    for(int k=0;k<m[0].size();k++)
-                        m[i][k] = 0;
-                    for(int k=0;k<m.size();k++)
-                        m[k][j] = 0;
+        unordered_set<int> x;
+        unordered_set<int> y;
+        for(int i=0; i<m.size();i++){
+            for(int j=0;j<m[i].size();j++){
+                if(m[i][j] == 0){
+                    x.insert(i);
+                    y.insert(j);
                 }
             }
+        }
+        for(auto a: x){
+            for(int i=0;i<m[a].size();i++)
+                m[a][i] = 0;
+        }
+        for(auto a: y){
+            for(int i=0;i<m.size();i++)
+                m[i][a] = 0;
         }
     }
 };
