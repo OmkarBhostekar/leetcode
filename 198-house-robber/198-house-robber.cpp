ccf452be618ec779687870e150e05a6c6b80e208
Omkar Bhostekar <59085919+OmkarBhostekar@users.noreply.github.com>
2022-06-07 21:43:34 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (25.69%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -3,14 +3,14 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int> dp(n);
-        dp[0] = nums[0];
-        if(n==1) return dp[0];
-        dp[1] = max(nums[0],nums[1]);
+        int prev2 = nums[0];
+        if(n==1) return prev2;
+        int prev = max(prev2,nums[1]);
         for(int i=2;i<n;i++){
-            int x = dp[i-1];
-            int y = nums[i] + dp[i-2];
-            dp[i] = max(x,y);
+            int x = max(prev,nums[i]+prev2);
+            prev2 = prev;
+            prev = x;
         }
-        return max(dp[n-1],dp[n-2]);
+        return max(prev2,prev);
     }
 };
