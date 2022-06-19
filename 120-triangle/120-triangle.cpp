a872c933aa8c679ce69aef043de3626e24cceeda
Omkar Bhostekar <59085919+OmkarBhostekar@users.noreply.github.com>
2022-06-19 14:32:53 +0530

    Time: 11 ms (43.45%), Space: 9.1 MB (21.38%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    int mem(vector<vector<int>>& t,vector<vector<int>> &dp,int i, int j){
-        if(i==t.size())
-            return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int x = t[i][j] + mem(t,dp,i+1,j);
-        int y = t[i][j] + mem(t,dp,i+1,j+1);
-        return dp[i][j] = min(x,y);
-    }
-    
     int minimumTotal(vector<vector<int>>& t) {
+        int n = t.size();
         vector<vector<int>> dp(t.size(),vector<int>(t.size(),-1));
-        return mem(t,dp,0,0);
+        dp[n-1] = t[n-1];
+
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                dp[i][j] = t[i][j]+ min(dp[i+1][j],dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
     }
 };
