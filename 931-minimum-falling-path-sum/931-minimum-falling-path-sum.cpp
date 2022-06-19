9797e5f15b919a39d3cea60c26175bc4cfbc77d2
Omkar Bhostekar <59085919+OmkarBhostekar@users.noreply.github.com>
2022-06-19 15:23:21 +0530

    Time: 7 ms (99.70%), Space: 10.1 MB (61.03%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    int memo(vector<vector<int>>& mat, vector<vector<int>>& dp, int i, int j){
-        if(i==dp.size()-1) return dp[i][j] = mat[i][j];
-        if(dp[i][j] != -1) return dp[i][j];
-        int l=INT_MAX,d=INT_MAX,r=INT_MAX;
-        d = memo(mat, dp, i+1, j);
-        if(j>0) l = memo(mat, dp, i+1, j-1);
-        if(j<mat[0].size()-1) r = memo(mat, dp, i+1, j+1);
-        return dp[i][j] = (mat[i][j]+min(l,min(d,r)));
-    }
-
     int minFallingPathSum(vector<vector<int>>& mat) {
         int m = mat.size(), n = mat[0].size(),ans = INT_MAX;
         vector<vector<int>> dp(m, vector<int>(n, -1));
-        for(int j=0;j<n;j++){
-            ans = min(ans,memo(mat, dp, 0, j));
+        dp[m-1] = mat[m-1];
+        for(int i=m-2;i>=0;i--){
+            for(int j=0;j<n;j++){
+                int l=INT_MAX, r=INT_MAX;
+                if(j>0) l = dp[i+1][j-1];
+                if(j<n-1) r = dp[i+1][j+1];
+                dp[i][j] = mat[i][j] + min(dp[i+1][j], min(l,r));
+            }
         }
+        for(int j=0;j<n;j++)
+            ans = min(ans, dp[0][j]);
         return ans;
     }
 };
