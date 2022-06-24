3e22799542eccdc5255faeee80a325d70c8ec865
Omkar Bhostekar <59085919+OmkarBhostekar@users.noreply.github.com>
2022-06-25 01:12:07 +0530

    Time: 59 ms (65.71%), Space: 14.7 MB (31.36%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -8,25 +8,13 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *l1, ListNode *l2) {
-        unordered_set<ListNode*> st;
-        while(l1 && l2){
-            if(l1 == l2) return l1;
-            if(st.find(l1) != st.end()) return l1;
-            if(st.find(l2) != st.end()) return l2;
-            st.insert(l1);
-            st.insert(l2);
-            l1 = l1->next;
-            l2 = l2->next;
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* dummy1 = headA;
+        ListNode* dummy2 = headB;
+        while(dummy1 != dummy2){
+            dummy1 = dummy1 == NULL ? headB : dummy1->next;
+            dummy2 = dummy2 == NULL ? headA : dummy2->next;
         }
-        while(l1){
-            if(st.find(l1) != st.end()) return l1;
-            l1 = l1->next;
-        }
-        while(l2){
-            if(st.find(l2) != st.end()) return l2;
-            l2 = l2->next;
-        }
-        return NULL;
+        return dummy1;
     }
 };
