f6c04d94e8f3934c47e414550dc6518c725b30b8
Omkar Bhostekar <59085919+OmkarBhostekar@users.noreply.github.com>
2022-06-24 22:01:26 +0530

    Time: 75 ms (19.44%), Space: 71.3 MB (46.85%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
@@ -24,35 +24,26 @@ public:
     }
     
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        stack<int> s1,s2;
         int carry=0, sum=0;
         ListNode* dummy = new ListNode(0);
         ListNode* cur=dummy;
-        
-        while(l1){
-            s1.push(l1->val);
-            l1 = l1->next;
-        }
-        while(l2){
-            s2.push(l2->val);
-            l2 = l2->next;
-        }
-        while(!s1.empty() or !s2.empty()){
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+        while(l1 or l2 or carry){
             sum=carry;
-            if(!s1.empty()){
-                sum += s1.top();
-                s1.pop();
+            if(l1){
+                sum += l1->val;
+                l1 = l1->next;
             }
-            if(!s2.empty()){
-                sum += s2.top();
-                s2.pop();
+            if(l2){
+                sum += l2->val;
+                l2 = l2->next;
             }
             carry = sum/10;
             sum %= 10;
             cur->next = new ListNode(sum);
             cur = cur->next;
         }
-        if(carry) cur->next = new ListNode(carry);
         return reverse(dummy->next);
     }
 };
