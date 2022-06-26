a82edafe20ecd25b019f1acd84fb67d74b6ae2ab
Omkar Bhostekar <59085919+OmkarBhostekar@users.noreply.github.com>
2022-06-27 00:43:44 +0530

    Time: 3 ms (74.51%), Space: 9.1 MB (71.57%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -4,24 +4,16 @@ public:
         int i=m-1,j=n-1,k=m+n-1;
         while(i>=0 and j>=0){
             if(nums1[i]>=nums2[j]){
-                nums1[k] = nums1[i];
-                i--;
-                k--;
+                nums1[k--] = nums1[i--];
             }else{
-                nums1[k] = nums2[j];
-                j--;
-                k--;
+                nums1[k--] = nums2[j--];
             }
         }
         while(i>=0){
-            nums1[k] = nums1[i];
-            i--;
-            k--;
+            nums1[k--] = nums1[i--];
         }
         while(j>=0){
-            nums1[k] = nums2[j];
-            j--;
-            k--;
+            nums1[k--] = nums2[j--];
         }
     }
 };
