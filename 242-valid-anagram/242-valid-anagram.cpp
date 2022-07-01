10b2905e07d74d269a12cb2b8d39a4d9f826eb71
Omkar Bhostekar <59085919+OmkarBhostekar@users.noreply.github.com>
2022-07-01 19:50:19 +0530

    Time: 4 ms (95.18%), Space: 7.4 MB (45.46%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int> m;
-        for(auto x: s) m[x]++;
-        for(auto x: t) m[x]--;
-        for(auto x: m) if(x.second!=0) return false;
+        vector<int> arr(26,0);
+        for(auto x: s) arr[x-'a']++;
+        for(auto x: t) arr[x-'a']--;
+        for(auto x: arr) if(x!=0) return false;
         return true;
     }
 };
