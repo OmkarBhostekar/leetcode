0b642d090bd3b1fa8aae80fad573d6744507e91b
Omkar Bhostekar <59085919+OmkarBhostekar@users.noreply.github.com>
2022-07-03 00:28:55 +0530

    Time: 49 ms (85.88%), Space: 27.3 MB (98.72%) - LeetHub

---
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& target) {
+        long long sum=0;
+        int max=0;
+        for(int i=0;i<target.size();i++){
+            sum += target[i];
+            if(target[max]<target[i]) max = i;
+        }
+        
+        long diff = sum - target[max];
+        if(target[max] == 1 || diff == 1) return true;
+        
+        if(diff>target[max] || diff == 0 || target[max]%diff == 0) return false;
+        
+        target[max] %= diff;
+        
+        return isPossible(target);
+    }
+};
