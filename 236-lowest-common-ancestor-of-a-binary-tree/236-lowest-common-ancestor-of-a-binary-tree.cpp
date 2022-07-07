8c25aeec5983a98bdc6954138bfb338fb0d579b0
Omkar Bhostekar <59085919+OmkarBhostekar@users.noreply.github.com>
2022-07-08 02:07:45 +0530

    Time: 25 ms (48.98%), Space: 14.1 MB (85.93%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,29 +9,15 @@
  */
 class Solution {
 public:
-    void preOrder(TreeNode* root, TreeNode* p, TreeNode* q, vector<TreeNode*> &path, vector<TreeNode*> &pp, vector<TreeNode*> &pq){
-        if(!root) return;
-        path.push_back(root);
-        if(root == p)
-            pp = path;
-        else if(root == q)
-            pq = path;
-        preOrder(root->left,p,q,path,pp,pq);
-        preOrder(root->right,p,q,path,pp,pq);
-        path.pop_back();
-        
-    }
-    
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> pp,pq,temp;
-        preOrder(root,p,q,temp,pp,pq);
-        TreeNode* lca = root;
-        int i=0;
-        while(i<pp.size() && i<pq.size()){
-            if(pp[i] != pq[i])
-                return lca;
-            lca = pp[i++];
-        }
-        return lca;
+        if(!root || root==p || root==q) return root;
+        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
+        
+        if(left == NULL)
+            return right;
+        else if(right == NULL)
+            return left;
+        else return root;
     }
 };
