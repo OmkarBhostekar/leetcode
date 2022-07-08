d8500f44257f652fdbdce37d2894a8ae2a5c3dce
Omkar Bhostekar <59085919+OmkarBhostekar@users.noreply.github.com>
2022-07-08 16:35:14 +0530

    Time: 60 ms (47.93%), Space: 18.8 MB (94.14%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& in) {
-        vector<vector<int>> ans;
         sort(in.begin(),in.end());
+        vector<vector<int>> ans;
         pair<int,int> cur = {in[0][0],in[0][1]};
         for(int i=1;i<in.size();i++){
-            if(in[i][0]>=cur.first && in[i][0] <= cur.second){
+            if((in[i][0]>=cur.first && in[i][0] <= cur.second) || (in[i][1]>=cur.first && in[i][1] <= cur.second) || (cur.first>=in[i][0] && cur.first <= in[i][1]) || (cur.second>=in[i][0] && cur.second <= in[i][1])){
                 cur.first = min(cur.first,in[i][0]);
                 cur.second = max(cur.second,in[i][1]);
             }else{
