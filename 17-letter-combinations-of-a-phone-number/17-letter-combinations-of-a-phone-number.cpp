6cb6740d2a195c7dcd9293885712780eaa0f7a75
Omkar Bhostekar <59085919+OmkarBhostekar@users.noreply.github.com>
2022-07-13 23:24:26 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (34.15%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
+    vector<vector<char>> dict = {
+        {},{'a','b','c'},{'d','e','f'},{'g','h','i'},{'j','k','l'},{'m','n','o'},{'p','q','r','s'},{'t','u','v'},{'w','x','y','z'}
+    };
     
-    void letterHelper(string digits, vector<vector<char>> &keys, vector<string> &ans, string temp, int i, int n){
-        if(i==n){
+    void rec(string digits, int i, string temp, vector<string> &ans){
+        if(i == digits.size()){
             ans.push_back(temp);
             return;
         }
-        for(char x: keys[digits[i]-'0']){
-            letterHelper(digits, keys, ans, (temp+x), i+1, n);
+        for(char c: dict[digits[i]-'0'-1]){
+            rec(digits, i+1, temp+c,ans);
         }
     }
     
     vector<string> letterCombinations(string digits) {
         vector<string> ans;
         if(digits.empty()) return ans;
-        
-        vector<vector<char>> keys = {
-            {},{},{ 'a','b','c' }, { 'd','e','f' }, { 'g','h','i' }, { 'j','k','l' }, 
-            { 'm','n','o' }, { 'p','q','r','s' }, { 't','u','v' }, { 'w','x','y','z' }
-        };
-        
-        letterHelper(digits, keys, ans, "", 0, digits.size());
+        rec(digits, 0, "", ans);
         return ans;
     }
 };
