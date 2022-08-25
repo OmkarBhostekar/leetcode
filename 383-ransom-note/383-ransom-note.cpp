101a6b2d45a92d76e231ddcf5263df83a46459a9
Omkar Bhostekar <59085919+OmkarBhostekar@users.noreply.github.com>
2022-08-25 14:07:26 +0530

    Time: 16 ms (80.72%), Space: 8.7 MB (89.81%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        vector<int> v1(26,0),v2(26,0);
-        for(auto ch: ransomNote) v1[ch-'a']++;
-        for(auto ch: magazine) v2[ch-'a']++;
+        vector<int> v(26,0);
+        for(auto ch: magazine) v[ch-'a']++;
+        for(auto ch: ransomNote) v[ch-'a']--;
         for(int i=0;i<26;i++){
-            if(v1[i]>v2[i]) return false;
+            if(v[i]<0) return false;
         }
         return true;
     }
