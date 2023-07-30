e253b23ab2ca9b82d2056ebf1f91fd6b3465608d
Omkar Bhostekar <59085919+OmkarBhostekar@users.noreply.github.com>
2023-07-30 13:59:55 +0530

    Time: 0 ms (100%), Space: 20.5 MB (33.33%) - LeetHub

---
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.cpp
@@ -0,0 +1,6 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int ans = 0; for(auto x: hours) if(x>=target) ans++; return ans;
+    }
+};
