cdbe044fc82050b3a228f4f4c419301f75afb15a
Omkar Bhostekar <59085919+OmkarBhostekar@users.noreply.github.com>
2023-08-11 18:36:45 +0530

    Time: 8 ms (92%), Space: 7.4 MB (81.57%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,19 +2,14 @@ class Solution {
 public:
     int change(int a, vector<int>& v) {
         int n = v.size();
-        vector<vector<long long>> dp(a+1, vector<long long>(n+1,-1LL));
         sort(v.begin(),v.end());
-        function<long long(int,int)> f = [&](int am, int i){
-            if(am==0) return 1LL;
-            if(i==n || am<0) return 0LL;
-            if(dp[am][i] != -1) return dp[am][i];
-            long long ans = 0;
-            for(int j=i;j<n;j++){
-                if(v[i]>am) break;
-                ans += f(am-v[j],j);
+        vector<long long> dp(a+1, 0LL);
+        dp[0] = 1LL;
+        for(auto c: v){
+            for(int am=c;am<=a;am++){
+                dp[am] += dp[am-c];
             }
-            return dp[am][i] = ans;
-        };
-        return f(a,0);
+        }
+        return dp[a];
     }
 };
