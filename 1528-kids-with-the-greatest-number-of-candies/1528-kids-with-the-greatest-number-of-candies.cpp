386a6a238a2612ffbaed4ab1ecd055953ad95980
Omkar Bhostekar <59085919+OmkarBhostekar@users.noreply.github.com>
2023-08-22 14:43:29 +0530

    Time: 5 ms (24.77%), Space: 8.9 MB (31.78%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.cpp
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& c, int ec) {
-        multiset<int> st;
-        for(auto x: c) st.insert(x);
+        int maxi = *max_element(c.begin(),c.end());
         vector<bool> ans((int)c.size(),false);
         for(int i=0;i<ans.size();i++){
-            st.erase(st.lower_bound(c[i]));
-            int maxi = *st.rbegin();
             ans[i] = c[i]+ec >= maxi;
-            st.insert(c[i]);
         }
         return ans;
     }
